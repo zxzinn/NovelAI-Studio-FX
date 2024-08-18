9c52c7b3f65ad7669467ce884f0475a8019d50a0
zhaoxinn <zhaoxinzhang0429@gmail.com>
2024-08-18 22:24:35 +0800

    New theme attempt

--- src/main/java/com/zxzinn/novelai/Application.java
+++ src/main/java/com/zxzinn/novelai/Application.java
@@ -1,6 +1,8 @@
 package com.zxzinn.novelai;
 
+import atlantafx.base.theme.NordDark;
 import atlantafx.base.theme.PrimerDark;
+import atlantafx.base.theme.PrimerLight;
 import com.google.gson.Gson;
 import com.zxzinn.novelai.api.APIClient;
 import com.zxzinn.novelai.component.NotificationPane;
