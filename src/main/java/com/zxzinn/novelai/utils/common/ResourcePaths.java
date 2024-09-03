92e192764a1f0380a6e38879b4825cbed571791e
zhaoxinn <zhaoxinzhang0429@gmail.com>
2024-09-03 23:06:54 +0800

    Deleted ControlBar # TODO Check if there is any buggy functions Implement test units Enhance metadata reader stylesheet Fix buggy Embed autocomplete/detection

--- src/main/java/com/zxzinn/novelai/utils/common/ResourcePaths.java
+++ src/main/java/com/zxzinn/novelai/utils/common/ResourcePaths.java
@@ -10,7 +10,6 @@ public final class ResourcePaths {
     public static final String MAIN_VIEW_FXML = FXML_ROOT + "MainView.fxml";
     public static final String FILE_MANAGER_FXML = FXML_ROOT + "filemanager/FileManager.fxml";
     public static final String IMAGE_GENERATOR_FXML = FXML_ROOT + "generator/ImageGenerator.fxml";
-    public static final String IMAGE_CONTROL_BAR_FXML = FXML_ROOT + "generator/ImageControlBar.fxml";
     public static final String HISTORY_IMAGE_PANE = FXML_ROOT + "generator/HistoryImagesPane.fxml";
     public static final String PROMPT_CONTROLS = FXML_ROOT + "generator/PromptControls.fxml";
     public static final String PROMPT_AREA = FXML_ROOT + "generator/PromptArea.fxml";
