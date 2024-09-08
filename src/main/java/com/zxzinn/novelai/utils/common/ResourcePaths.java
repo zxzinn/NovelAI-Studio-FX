5d04c307d549c3e0b86c0e21f0ecb8e4a164ef8e
zhaoxinn <zhaoxinzhang0429@gmail.com>
2024-09-09 05:39:23 +0800

    Optimized ResourcePaths # TODO Perform new modeled Generation Tasks

--- src/main/java/com/zxzinn/novelai/utils/common/ResourcePaths.java
+++ src/main/java/com/zxzinn/novelai/utils/common/ResourcePaths.java
@@ -1,7 +1,5 @@
 package com.zxzinn.novelai.utils.common;
 
-import javafx.fxml.FXML;
-
 public final class ResourcePaths {
     public static final String FXML_ROOT = "/com/zxzinn/novelai/fxml/";
     public static final String CSS_ROOT = "/com/zxzinn/novelai/css/";
@@ -9,10 +7,8 @@ public final class ResourcePaths {
     public static final String EXECUTABLE_ROOT = "/com/zxzinn/novelai/executable/";
     public static final String ICON_PATH = "/com/zxzinn/novelai/icon/";
 
-    public static final String MAIN_VIEW_FXML = FXML_ROOT + "MainView.fxml";
     public static final String TASK_MONITOR_FXML = FXML_ROOT + "task_monitor.fxml";
     public static final String FILE_MANAGER_FXML = FXML_ROOT + "filemanager/FileManager.fxml";
-    public static final String IMAGE_GENERATOR_FXML = FXML_ROOT + "generator/ImageGenerator.fxml";
 
     public static final String META_READER_PATH = EXECUTABLE_ROOT + "meta_reader.exe";
 
