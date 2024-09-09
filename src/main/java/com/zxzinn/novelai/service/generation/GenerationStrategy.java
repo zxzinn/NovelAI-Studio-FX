422c57ae3b9fb1c8d231520350abe062c585dd68
zhaoxinn <zhaoxinzhang0429@gmail.com>
2024-09-09 19:32:25 +0800

    Removed redundant classes # TODO Perform new modeled Generation Tasks

--- src/main/java/com/zxzinn/novelai/service/generation/GenerationStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.zxzinn.novelai.service.generation;
-
-
-import com.zxzinn.novelai.api.GenerationPayload;
-
-public interface GenerationStrategy {
-    GenerationPayload createPayload(GenerationParameters params);
-}
